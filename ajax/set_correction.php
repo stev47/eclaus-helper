802f1b53575712d35c34de57fa3c0b12dd304ba5
Stephan Hilb <stephan@ecshi.net>
2013-05-12 14:20:49 +0200

    group-grading attribut erstellen, falls nicht vorhanden

--- ajax/set_correction.php
+++ ajax/set_correction.php
@@ -10,6 +10,9 @@ if (isset($_POST['correction'])) {
 	file_put_contents($path, \ForceUTF8\Encoding::toUTF8($_POST['correction']));
 
 	$points = file_get_contents($path_points);
+	if (!preg_match('/^GROUP-GRADING\=.*$/m', $points)) {
+		$points .= "GROUP-GRADING=\n";
+	}
 	$points = preg_replace('/^(GROUP-GRADING\=).*$/m', '${1}' . $_POST['group_points'], $points);
 	echo $points;
 	file_put_contents($path_points, $points);
