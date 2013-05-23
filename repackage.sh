49a8f262cce75e6156365891e38054a0a95a1e54
Stephan Hilb <stephan@ecshi.net>
2013-05-23 17:31:05 +0200

    repackage: include .identity.eclaus hierarchy even in subdirs

--- repackage.sh
+++ repackage.sh
@@ -10,7 +10,16 @@ if [ -z "$subdir" ]; then
 	exit;
 fi
 
+rm -rf $output;
 find $subdir -name "points.eclaus.txt" -o -name "correction.eclaus.txt" -o -name ".identity.eclaus" \
-	| zip -q $output -@
+	| zip -q $output -@;
+
+idents=$(find $datadir -name ".identity.eclaus" | sed -r 's/\/\.identity\.eclaus$//');
+for ident in $idents; do
+	if [[ $subdir = $ident* ]]; then
+		zip -q $output "$ident/.identity.eclaus";
+	fi
+done;
+
 
 
